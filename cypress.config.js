7b880c064b4ec72a6f8f0287b1aac9bb0068eecc
Carlos Eduardo Rovai <carloseduardorovai@outlook.com>
2025-04-28 13:27:33 -0300

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
     },
     chromewebSecurity: false,
     e2e: {
-        baseUrl: 'https://www.saucedemo.com/', 
+        baseUrl: 'https://www.saucedemo.com', 
         setupNodeEvents(on, config) {
             require('cypress-mochawesome-reporter/plugin')(on);
         },
